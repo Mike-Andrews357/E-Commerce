ddfbe65b3529c15c7b920809147997d26c540841
Michael Andrews <mike.andrews357@gmail.com>
2020-04-10 20:04:24 -0400

    Broken our homepage into smaller components, and finshed styling our menu items

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
-
 import './App.css';
 
+import HomePage from './pages/homepage/homepage.component';
+
 function App() {
-	return <div Hello World />;
+	return (
+		<div>
+			<HomePage />
+		</div>
+	);
 }
 
 export default App;
